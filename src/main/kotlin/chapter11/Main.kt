dd04ec687060629f9b47519088ef4012f3a77a63
hiro032 <kiryuinh32@gmail.com>
2022-10-30 22:59:11 +0900

    11. 코틀린에서 접근 제어를 다루는 방법

---
+++ src/main/kotlin/chapter11/Main.kt
@@ -0,0 +1,14 @@
+package chapter11
+
+fun isDirectoryPath(path: String): Boolean {
+    return path.endsWith("/")
+}
+
+class Car (
+    internal val name: String,
+    price: Int
+) {
+    var price = price
+        private set
+}
+
